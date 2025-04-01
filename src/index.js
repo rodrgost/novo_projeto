d785e2b294e47db29ea96ed6677ffa9b5bff5167
rodrgost <rodrgost@gmail.com>
2025-04-01 08:08:02 -0300

    Remove unnecessary Express server code from index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Importar rotas
+const indexRouter = require('./routes/index');
+
+// Usar rotas
+app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
